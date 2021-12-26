c4d9690711f5e7665a7969bd3c69dc68e4357036
Kurolius <ymajbar.pro@gmail.com.com>
2021-12-26 21:03:41 +0100

    Beta1.1

---
+++ src/Entity/Auteur.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuteurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AuteurRepository::class)
+ */
+class Auteur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_prenom;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_naissance;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nationalite;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Livre::class, inversedBy="Auteurs")
+     */
+    private $Livre;
+
+    public function __construct()
+    {
+        $this->Livre = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomPrenom(): ?string
+    {
+        return $this->nom_prenom;
+    }
+
+    public function setNomPrenom(string $nom_prenom): self
+    {
+        $this->nom_prenom = $nom_prenom;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getDateDeNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_de_naissance;
+    }
+
+    public function setDateDeNaissance(\DateTimeInterface $date_de_naissance): self
+    {
+        $this->date_de_naissance = $date_de_naissance;
+
+        return $this;
+    }
+
+    public function getNationalite(): ?string
+    {
+        return $this->nationalite;
+    }
+
+    public function setNationalite(string $nationalite): self
+    {
+        $this->nationalite = $nationalite;
+
+        return $this;
+    }
+    public function __toString()
+    {
+        return $this->nom_prenom;
+    }
+
+    /**
+     * @return Collection|Livre[]
+     */
+    public function getLivre(): Collection
+    {
+        return $this->Livre;
+    }
+
+    public function addLivre(Livre $livre): self
+    {
+        if (!$this->Livre->contains($livre)) {
+            $this->Livre[] = $livre;
+        }
+
+        return $this;
+    }
+
+    public function removeLivre(Livre $livre): self
+    {
+        $this->Livre->removeElement($livre);
+
+        return $this;
+    }
+}
